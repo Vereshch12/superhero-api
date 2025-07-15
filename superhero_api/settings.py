ec951c66c267325403c1826a498e3c417aa7c25a
Mikhail Vereshchagin <farasers@mai.ru>
2025-07-16 00:17:04 +0300

    Setup project structure, virtualenv, and GitHub integration

--- superhero_api/settings.py
+++ superhero_api/settings.py
@@ -11,19 +11,16 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&ugsx327g82fybk4lle_*nml3f0xmqat2d9c9by!++*r78yk*4'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = []
 
